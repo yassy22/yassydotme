7a1ab268a1ba550b0f42986e576b3f3e965ddceb
yassy22 <99503819+yassy22@users.noreply.github.com>
2025-11-11 15:00:57 +0100

    fix

--- src/app/components/FinalYear.tsx
+++ src/app/components/FinalYear.tsx
@@ -1,23 +1,23 @@
-import home from "../api/home/home";
-import { motion, useInView } from "framer-motion";
-import Image from "next/image";
+"use client";
 import React, { useRef } from "react";
+import Image from "next/image";
+import { motion, type Variants, useInView, cubicBezier } from "framer-motion";
+import home from "../api/home/home";
 
 function FinalYear() {
-  const imageRef = useRef(null);
+  const imageRef = useRef<HTMLDivElement | null>(null);
   const isInView = useInView(imageRef, { once: true });
 
-  const EASING: any = [0.83, 0, 0.17, 1];
+  // Typesafe easing i.p.v. `any`
+  const easing = cubicBezier(0.83, 0, 0.17, 1);
 
-  const reveal = {
-    initial: {
-      height: "0%",
-    },
+  const reveal: Variants = {
+    initial: { height: "0%" },
     animate: {
       height: "auto",
       transition: {
         duration: 0.9,
-        ease: EASING,
+        ease: easing,
         delay: 1,
       },
     },
@@ -26,13 +26,13 @@ function FinalYear() {
   return (
     <section className="mx-4 my-40 sm:mx-[130px] sm:h-[140vh] relative">
       <motion.div
+        ref={imageRef}
+        className="overflow-hidden absolute top-60 right-0 hidden lg:block"
         variants={reveal}
         initial="initial"
-        className="overflow-hidden absolute top-60 right-0 hidden lg:block "
         animate={isInView ? "animate" : "initial"}
-        ref={imageRef}
       >
-        <motion.div className="">
+        <motion.div>
           <Image
             src={home[0].image[2]}
             alt="Fixed Size Image"
@@ -43,12 +43,12 @@ function FinalYear() {
         </motion.div>
       </motion.div>
 
-      <div className=" h-[70vh] sm:h-screen flex flex-col gap-10 justify-center items-center relative z-10">
+      <div className="h-[70vh] sm:h-screen flex flex-col gap-10 justify-center items-center relative z-10">
         <p className="text-[14px] sm:text-[45px] font-light">
           In my final year of studies, I am dedicated to further honing my
           skills in front-end development and design.
         </p>
-        <p className="text-[14px] sm:text-[45px] ">
+        <p className="text-[14px] sm:text-[45px]">
           I focus on transforming creative ideas into well-crafted, high-quality
           products from concept to completion.
         </p>
